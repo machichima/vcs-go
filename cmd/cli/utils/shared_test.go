27424ac29ae8defe5da46c30e36ac2b2690ce716
machichima <nary12321@gmail.com>
2024-08-30 00:50:50 +0100

    feat: create temp files in .vgo/objects for test
    follow the create objects rule

--- cmd/cli/utils/shared_test.go
+++ cmd/cli/utils/shared_test.go
@@ -3,23 +3,45 @@ package utils_test
 import (
 	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/machichima/vcs-go/cmd/cli/utils"
 )
 
 const TestStr = `This is a test
 second line`
 
 func CreateTempFile(dir string) (string, error) {
-    path := dir + "/test.txt"
+	path := dir + "/test.txt"
 
-    err := os.WriteFile(path, []byte(TestStr), os.ModePerm)
+	err := os.WriteFile(path, []byte(TestStr), os.ModePerm)
 	if err != nil {
 		log.Fatal("Error occur while writing temp content to file")
 	}
 
-    return path, nil
+	return path, nil
 }
 
+// Create a temp file in the .vgo/objects/hash[:2] dir
+// return the full path for hash file
+func CreateTempFileInVgo(dir string) (string, error) {
 
-func CreateTempRepo() {
-    // TODO: Create a temp repo
+	hash := "dfq8hffjalkgihboq"
+	filePath := filepath.Join(dir, utils.ObjectsDirName, hash[:2], hash)
+
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		return "", err
+	}
+
+	if err := os.WriteFile(filePath, []byte(TestStr), os.ModePerm); err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
+
+// create a temp .vgo in the dir with numOfFile files
+// return the repo path
+func CreateTempVgo(dir string, numOfFile int) {
+	// TODO: Create a temp repo
 }
