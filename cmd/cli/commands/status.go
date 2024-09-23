8a41c12b63a8e7d103401c999581443250cf1307
machichima <nary12321@gmail.com>
2024-09-23 22:26:40 +0800

    feat: enable check status of deleted files

--- cmd/cli/commands/status.go
+++ cmd/cli/commands/status.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/machichima/vcs-go/cmd/cli/utils"
 )
@@ -12,32 +13,130 @@ import (
 // 1. check if index file exists (if not, print no staged and return)
 // 2. get the bytes content in the index file
 // 3. deserialize the bytes content into Index struct
+// stages:
+// - staged files
+//   - modified (in index)
+//   - Removed files (not in file tree)
+//
+// - Modifications Not Staged For Commit (in commit filetree)
+//   - modified
+//   - deleted
+//
+// - Untracked Files (not in file tree)
 func executeStatus() error {
 
-    index, err := utils.ReadIndexFile()
-    if err != nil {
-        if !errors.Is(err, os.ErrNotExist) {
-            return err
-        }
-    }
+	index, err := utils.ReadIndexFile()
+	if err != nil {
+		if !errors.Is(err, os.ErrNotExist) {
+			return err
+		}
+	}
 
-    fmt.Println("Changes to be committed:")
-    for file, _ := range index.FileToHash {
-        fmt.Println(file)
-    }
+	isFirstCommit := false
 
-    files, err := utils.GetFiles("./")
-    if err != nil {
-        return err
-    }
+	commitHashByte, err := os.ReadFile(utils.HEADFileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			isFirstCommit = true
+		} else {
+			return err
+		}
+	}
 
-    fmt.Println("Changes not staged for commit:")
-    for _, file := range files {
-        _, ok := index.FileToHash[file]
-        if !ok {
-            fmt.Println(file)
-        }
-    }
+	var modifiedFiles []string
+	var newFiles []string
+	var deletedFiles []string
+	var stagedModifiedFiles []string
+	var stagedNewFiles []string
+	var stagedDeletedFiles []string
 
-    return nil
+	// Go through the filetree of prev commit
+	if !isFirstCommit {
+		commitHash := string(commitHashByte)
+
+		// get the commit struct by commitHash
+		commit, err := utils.ReadCommit(commitHash)
+		if err != nil {
+			return err
+		}
+
+		// Receive the hash of the file with path same as filePath
+		fileTreeHash := commit.FileTree
+		fileTree, err := utils.ReadFileTree(fileTreeHash)
+		if err != nil {
+			return err
+		}
+
+		// get all files in the workspace
+		files, err := utils.GetFiles("./")
+		if err != nil {
+			return err
+		}
+
+		// Not staged (modified)
+		for _, file := range files {
+
+			fileStatus, err := utils.CompareFileToFileTree(file, fileTree)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(fileStatus)
+
+            // file is staged
+			if _, ok := index.FileToHash[file]; ok {
+				if fileStatus == utils.NewFile {
+					stagedNewFiles = append(stagedNewFiles, file)
+				} else if fileStatus == utils.ModifiedFile {
+					stagedModifiedFiles = append(stagedModifiedFiles, file)
+				}
+			} else {
+
+				if fileStatus == utils.NewFile {
+					newFiles = append(newFiles, file)
+				} else if fileStatus == utils.ModifiedFile {
+					modifiedFiles = append(modifiedFiles, file)
+				}
+
+			}
+
+		}
+
+		for file, _ := range fileTree.FileToHash {
+			if !slices.Contains(files, file) {
+				// file is deleted since last commit
+
+                if _, ok := index.FileToHash[file]; ok {
+                    stagedDeletedFiles = append(stagedDeletedFiles, file)
+                } else {
+                    deletedFiles = append(deletedFiles, file)
+                }
+			}
+		}
+
+	}
+
+	fmt.Println("Staged: ")
+	for _, file := range stagedNewFiles {
+		fmt.Println(file, " (new)")
+	}
+	for _, file := range stagedModifiedFiles {
+		fmt.Println(file, " (modified)")
+	}
+	for _, file := range stagedDeletedFiles {
+		fmt.Println(file, " (deleted)")
+	}
+
+	fmt.Println("Modifications Not Staged For Commit: ")
+	for _, file := range newFiles {
+		fmt.Println(file, " (new)")
+	}
+	for _, file := range modifiedFiles {
+		fmt.Println(file, " (modified)")
+	}
+	for _, file := range deletedFiles {
+		fmt.Println(file, " (deleted)")
+	}
+
+	return nil
 }
