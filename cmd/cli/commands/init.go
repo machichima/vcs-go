4c920f8dfa624adf2c52410d1551a13068929567
machichima <nary12321@gmail.com>
2024-10-12 13:31:51 +0800

    fix: init main branch in init cmd

--- cmd/cli/commands/init.go
+++ cmd/cli/commands/init.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/machichima/vcs-go/cmd/cli/utils"
 )
@@ -12,18 +13,25 @@ func executeInit() error {
 	if exists, err := utils.CheckPathExists(utils.RootDirName); err != nil {
 		return err
 	} else if exists {
-        fmt.Println("repository already initialized")
+		fmt.Println("repository already initialized")
 	}
 
 	if err := utils.CreateDirs(utils.RootDirName, utils.ObjectsDirName, utils.RefsDirName); err != nil {
 		return err
 	}
 
-    // create HEAD dir ref to main branch
-    HEADContent := "main"
-    if err := os.WriteFile(utils.HEADFileName, []byte(HEADContent), os.ModePerm); err != nil {
-        return err
-    }
+	// create HEAD dir ref to main branch
+	HEADContent := "main"
+	if err := os.WriteFile(utils.HEADFileName, []byte(HEADContent), os.ModePerm); err != nil {
+		return err
+	}
+
+	// create main branch file
+	if err := os.WriteFile(
+		filepath.Join(utils.RefsDirName, HEADContent), []byte{}, os.ModePerm,
+	); err != nil {
+		return err
+	}
 
 	// fmt.Println("Initialized empty vcs-go repository")
 
